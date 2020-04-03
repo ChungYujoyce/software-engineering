53626e0610013f321db49f56ca13053c6d4538c5
graceHunag <dyna.ghieds12388@gmail.com>
2020-04-03 19:44:47 +0800

    Create UserModeTest.java

---
+++ GradeSystem2/GradeSystem/src/Mypackage/UserModeTest.java
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Main Class File:  UserModeTest.java
+// Semester:         Spring 2020
+// Course:			 Software Engineering
+//
+// Author:           Huang Chung Yu
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package Mypackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+/**
+ * Grades object testing.
+ * Test i user input ID(correct/not correct) or Q(leave)
+ * 
+ * Bugs: none known
+ * 
+ * @author       Huang Chung Yu
+ * @version      1.0
+ */
+class UserModeTest {
+	static ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Test
+	/** ------------------------------------------------------------------------------
+	 * test testUser_mode()
+	 * This test is for UserMode class 
+	 * 
+	 * (screen)		Enter ID of Q(finish usage)?
+	 * (user input)	Q						[leave system]
+	 * (screen)		Thanks for using!
+	 * 
+	 * input - (user input) "Q\n"
+	 * 
+	 * expected - (screen display) "Enter ID or Q(finish usage)?Thanks for using!\n"
+	 */
+	void testUser_mode_1() {
+		ByteArrayInputStream in = new ByteArrayInputStream("Q\n".getBytes());
+		System.setIn(in);
+		outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));		
+		UserMode.user_mode();
+		
+		assertEquals("Enter ID or Q(finish usage)?\n", outContent.toString());
+		
+		System.setIn(System.in);
+		System.setOut(null);
+	}
+	
+	@Test
+	void testUser_mode_2() {
+		ByteArrayInputStream in = new ByteArrayInputStream("962001051\n".getBytes());
+		System.setIn(in);
+		outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));		
+		UserMode.user_mode();
+		
+		assertEquals("Enter ID or Q(finish usage)?\n", outContent.toString());
+		
+	}
+	
+	@Test
+	void testUser_mode_3() {
+		ByteArrayInputStream in = new ByteArrayInputStream("12333\n962001051\n".getBytes());
+		System.setIn(in);
+		outContent = new ByteArrayOutputStream(); 
+		System.setOut(new PrintStream(outContent));	
+		System.setIn(System.in); 
+		UserMode.user_mode(); 
+		assertEquals("Enter ID or Q(finish usage)?\nSorry, your ID was wrong, please try again~\n", outContent.toString()); 
+	}
+
+}
