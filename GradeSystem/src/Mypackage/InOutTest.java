8e62738bb341e7fa80b801a59c4e693d1cb65e2e
graceHunag <joyce.bhps@gmail.com>
2020-04-09 23:25:25 +0800

    add javadoc

--- GradeSystem/src/Mypackage/InOutTest.java
+++ GradeSystem/src/Mypackage/InOutTest.java
@@ -19,11 +19,38 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+/**
+ * 
+ * Test the choosing action function.
+ * 
+ * Bugs: none known
+ * 
+ * @author       Huang Chung Yu
+ * @version      1.0
+ */
 
 class InOutTest {
 
 	static ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	static Scanner s;
+	/** ------------------------------------------------------------------------------
+	 * test testchoose_1()
+	 * This test is for InOut class 
+	 * 
+	 * (screen)		Welcome~ ³\Öqµ®
+	 *				 1) Show Grade (enter: G) 
+	 *				 2) Show Rank (enter: R)
+	 *				 3) Show Average (enter: A)
+	 *				 4) Update Weights (enter: W)
+	 *				 5) Leave Menu (enter: E)
+	 * (user input)	A						[leave system]
+	 * (screen)		none
+	 * 
+	 * input - (user input) "A\n"
+	 * 
+	 * expected - none (return command back to main Class)
+	 * 
+	 * */
 	@Test
 	void testchoose_1() {
 		ByteArrayInputStream in = new ByteArrayInputStream("A\n".getBytes());
@@ -41,7 +68,24 @@ class InOutTest {
 		assertEquals(expected, actuals);
 		
 	}
-
+	/** ------------------------------------------------------------------------------
+	 * test testchoose_2()
+	 * This test is for InOut class 
+	 * 
+	 * (screen)		Welcome~ ³\Öqµ®
+	 *				 1) Show Grade (enter: G) 
+	 *				 2) Show Rank (enter: R)
+	 *				 3) Show Average (enter: A)
+	 *				 4) Update Weights (enter: W)
+	 *				 5) Leave Menu (enter: E)
+	 * (user input)	G						[leave system]
+	 * (screen)		none
+	 * 
+	 * input - (user input) "G\n"
+	 * 
+	 * expected - none (return command back to main Class)
+	 * 
+	 * */
 	@Test
 	void testchoose_2() {
 		ByteArrayInputStream in = new ByteArrayInputStream("G\n".getBytes());
--- GradeSystem/src/Mypackage/InputTest.java
+++ GradeSystem/src/Mypackage/InputTest.java
@@ -17,11 +17,39 @@ import java.io.PrintStream;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
+/**
+ * 
+ * Test the read data file function.
+ * 
+ * Bugs: none known
+ * 
+ * @author       Huang Chung Yu
+ * @version      1.0
+ */
 
 class InputTest {
 
 	static ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
+	/** ------------------------------------------------------------------------------
+	 * test testRead_1()
+	 * This test is for Input class 
+	 * 
+	 * Manaully input the target student information,
+	 * expect to get the correct scores and names.
+	 * 
+	 * input1 - "¶À´Â°¶"
+	 * 		(ID, name, lab1, lab2, lab3, mid-term, final-exam) 
+	 * input2 - '985002510, 0, 91, 97, 82, 98, 81'
+	 * 
+	 * expected1 - "¶À´Â°¶"
+	 * 		(ID, name, lab1, lab2, lab3, mid-term, final-exam) 
+	 * expected2 - '985002510, 0, 91, 97, 82, 98, 81'
+	 * 
+	 * input1 should equals expected1
+	 * input2 should equals expected2
+	 * 
+	 * */
 	@Test
 	void testRead_1() {
 		
@@ -36,7 +64,25 @@ class InputTest {
 		assertTrue(Arrays.equals(expected2, actuals2));
 		
 	}
-
+	/** ------------------------------------------------------------------------------
+	 * test testRead_2()
+	 * This test is for Input class 
+	 * 
+	 * Manaully input the target student information,
+	 * expect to get the correct scores and names.
+	 * 
+	 * input1 - "³\Öqµ®"
+	 * 		(ID, name, lab1, lab2, lab3, mid-term, final-exam) 
+	 * input2 - '975002039, 0, 97, 84, 87, 99, 89'
+	 * 
+	 * expected1 - "³\Öqµ®"
+	 * 		(ID, name, lab1, lab2, lab3, mid-term, final-exam) 
+	 * expected2 - '975002039, 0, 97, 84, 87, 99, 89'
+	 * 
+	 * input1 should equals expected1
+	 * input2 should equals expected2
+	 * 
+	 * */
 	@Test
 	void testRead_2() {
 		
